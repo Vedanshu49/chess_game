49e71b379fbd3aef4b08201887e055448bc6177d
Vedanshu49 <vedanshupandey4907@outlook.com>
2025-08-29 09:24:20 +0530

    Fix 19 [Fixing deployment error 2.0]

--- frontend/lib/supabasejs.js
+++ frontend/lib/supabasejs.js
@@ -8,6 +8,6 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: true,
-    storage: localStorage,
+    storage: typeof window !== 'undefined' ? localStorage : undefined,
   },
 });
